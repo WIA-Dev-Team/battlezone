d31b6368a625e708fd354390e4dd825a2f053349
unknown <aaron@.(none)>
2011-04-19 02:16:18 -0500

    added a header file and a sandbox development area for me and the virtual environment development

---
+++ Aaron/environment_sandbox/environment_sandbox/VirtualEnvironment.h
@@ -0,0 +1,24 @@
+/**
+ * @Author Aaron Parker
+ * @Date 04-18-2011
+ */
+
+#ifndef VIRTUAL_ENVIRONMENT_H
+#define VIRTUAL_ENVIRONMENT_H
+
+#include "Object.h"
+#include <Vector.h>
+
+class VirtualEnvironment
+{
+public:
+	VirtualEnvironment();
+	VirtualEnvironment(const VirtualEnvironment &env);
+	~VirtualEnvironment();
+
+	move();
+private:
+
+};
+
+#endif
