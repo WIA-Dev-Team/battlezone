fd78351d7e5a75951aa7a0b4225c6207aed50754
Ruhar <bhubler76@hotmail.com>
2011-04-20 20:39:25 -0500

    Created userinput class to capture q, space, enter, w,a,s, and d, still needs further documentation.

---
+++ WIA_Battlezone/WIA_Battlezone/UserInput.cpp
@@ -0,0 +1,78 @@
+/*	This file is part of WIA Battlezone.
+ *
+ *	WIA Battlezone is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	 WIA Battlezone is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with WIA Battlezone.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	@file UserInput.cpp
+ *	@headerfile UserInput.h "UserInput.h"
+ *	@author Ben Hubler
+ *	@date 4/20/2011
+ *	@version 1.0.1
+ */
+#include "UserInput.h"
+#include <Windows.h>
+/**
+ *	Default constructor
+ */
+UserInput::UserInput()
+{
+	inputkeys.push_back(UserInputKey::A_KEY);
+	inputkeys.push_back(UserInputKey::D_KEY);
+	inputkeys.push_back(UserInputKey::ENTER_KEY);
+	inputkeys.push_back(UserInputKey::Q_KEY);
+	inputkeys.push_back(UserInputKey::SPACE_KEY);
+	inputkeys.push_back(UserInputKey::S_KEY);
+	inputkeys.push_back(UserInputKey::W_KEY);
+	keysdown.clear();
+}
+
+void UserInput::updateKeysDown()
+{
+	keysdown.clear();
+	if(GetAsyncKeyState(UserInputKey::A_KEY))
+	{
+		keysdown.push_front(UserInputKey::A_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::D_KEY))
+	{
+		keysdown.push_front(UserInputKey::D_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::ENTER_KEY))
+	{
+		keysdown.push_front(UserInputKey::ENTER_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::Q_KEY))
+	{
+		keysdown.push_front(UserInputKey::Q_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::SPACE_KEY))
+	{
+		keysdown.push_front(UserInputKey::SPACE_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::S_KEY))
+	{
+		keysdown.push_front(UserInputKey::S_KEY);
+	}
+	if(GetAsyncKeyState(UserInputKey::W_KEY))
+	{
+		keysdown.push_front(UserInputKey::W_KEY);
+	}
+
+}
+list<UserInputKey::UserInputKey>* UserInput::getKeysDown()
+{
+	updateKeysDown();
+	return &keysdown;
+}
---
+++ WIA_Battlezone/WIA_Battlezone/UserInput.h
@@ -0,0 +1,59 @@
+/*	This file is part of WIA Battlezone.
+ *
+ *	WIA Battlezone is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	 WIA Battlezone is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with WIA Battlezone.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *	@file UserInput.h
+ */
+
+#ifndef USERINPUT_H
+#define USERINPUT_H
+
+#include <list>
+
+/** 
+ *	
+ *	@author Ben Hubler
+ *	@date 4/20/2011
+ *	@version 1.0.0
+ */
+namespace UserInputKey
+{
+	enum UserInputKey
+	{
+		Q_KEY = 0x51,
+		W_KEY = 0x57,
+		S_KEY = 0x53,
+		A_KEY = 0x41,
+		D_KEY = 0x44,
+		ENTER_KEY = 0x0D,
+		SPACE_KEY = 0x20
+	};
+}
+
+using namespace std;
+
+class UserInput
+{
+public:
+	UserInput();
+	list<UserInputKey::UserInputKey>* getKeysDown();
+	void updateKeysDown();
+private:
+	list<UserInputKey::UserInputKey> inputkeys;
+	list<UserInputKey::UserInputKey> keysdown;
+};
+
+#endif
